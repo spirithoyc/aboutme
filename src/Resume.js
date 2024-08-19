5372b1c73388f39567d16b55cd75cf5516fbec11
Spirit_Ho <spirit_ho@SpiritMacProM3.local>
2024-08-20 00:29:20 +0800

    add resume body

--- src/Resume.js
+++ src/Resume.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './styles/resume.css';
-import EducationSection from './EducationSection';
-import ExperienceSection from './ExperienceSection';
 
 const Resume = () => {
     return (
